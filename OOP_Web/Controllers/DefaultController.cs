2d99192eea7e5be50a8429e97018251b5f3a13b3
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-01-17 17:44:36 +0300

    15

--- OOP_Web/Controllers/DefaultController.cs
+++ OOP_Web/Controllers/DefaultController.cs
@@ -10,7 +10,7 @@ namespace OOP_Web.Controllers
         //    Class1 c = new Class1();
         //    c.Topla();
         //}
-    
+
         int Topla()
         {
             int s1 = 2;
@@ -23,7 +23,7 @@ namespace OOP_Web.Controllers
         {
             int kisaKenar = 10;
             int uzunKenar = 20;
-            int cevre=2*(kisaKenar+uzunKenar);
+            int cevre = 2 * (kisaKenar + uzunKenar);
             return cevre;
         }
         string Cumle()
@@ -38,21 +38,71 @@ namespace OOP_Web.Controllers
             ViewBag.m2 = "Merhaba Asp.Net ile yardırmaya devam";
             ViewBag.m3 = "Oop'nin içinden geçmeye devam";
         }
+
+        void MesajListesi(string p)
+        {
+            ViewBag.v = p;
+        }
+        void Kullaninici(string userName)
+        {
+            ViewBag.v = userName;
+        }
         public IActionResult Index()
         {
             Mesajlar();
+            MesajListesi("Paramtre ismi");
+            Kullaninici("By GolgeRoot");
+            ViewBag.x = Carp(22, 22);
             return View();
+        }
+
+        int Carp(int s1, int s2)
+        {
+            int sonuc = s1 * s2;
+            return sonuc;
+        }
+
+        int Faktoriyel(int p)
+        {
+            int f = 1;
+            for (int i = 1; i <= p; i++)
+            {
+                f = f * i;
+            }
+            return f;
         }
         public IActionResult Urunler()
         {
             Mesajlar();
-            ViewBag.t=Topla();
+            Kullaninici("Hacker GolgeRoot");
+            ViewBag.t = Topla();
             ViewBag.c = Cevre();
+            ViewBag.Faktor = Faktoriyel(9);
             return View();
         }
         public IActionResult Musteriler()
         {
             ViewBag.d = Cumle();
+            Kullaninici("GolgeRoot the is Hacker");
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            Kullaninici("Admin By Golgeroot");
+            return View();
+        }
+
+        public IActionResult Deneme()
+        {
+            Sehirler sehirler = new Sehirler();
+            sehirler.Ad = "Adana";
+            sehirler.ID = 1;
+            sehirler.Nufus = 4753000;
+            sehirler.Ulke = "Türkiye";
+            ViewBag.v1 = sehirler.Ad;
+            ViewBag.v2 = sehirler.ID;
+            ViewBag.v3 = sehirler.Nufus;
+            ViewBag.v4 = sehirler.Ulke;
             return View();
         }
     }
---
+++ OOP_Web/Ornekler/Sehirler.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Web.Ornekler
+{
+    public class Sehirler
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public int Nufus { get; set; }
+        public string Ulke { get; set; }
+    }
+}
